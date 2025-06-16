4e2e417d4819a381c6b9ed914e8b46157134b31d
drr00t <adribeiro@gmail.com>
2025-06-16 09:06:18 -0300

    initial commit

--- src/main/java/samza/examples/sql/samza-sql-filter/src/main/sql/samza.sql
+++
@@ -1,9 +0,0 @@
--- Filter Profile change-capture stream by 'Product Manager'
--- title and project basic profile data to a kafka topic.
-
-INSERT INTO kafka.ProductManagerProfiles
-SELECT memberId, firstName, lastName, company
-FROM kafka.ProfileChanges
-WHERE standardize(title) = 'Product Manager'
-
--- you can add additional SQL statements here
